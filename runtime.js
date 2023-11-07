ccfdc00b9c985fc29fd24b8eefe98193dad32fd0
Jacob Snarr <jacobsnarr@gmail.com>
2023-11-07 03:58:53 -0500

    Implement runtime and add control buttons

--- runtime.js
+++ runtime.js
@@ -1,11 +1,22 @@
-import { CHIP8_Emulator } from "./emulator";
+import { CHIP8_Emulator } from "./emulator.js";
+
+let runtime;
+let fileInput, stopButton, startButton, nextButton;
+
+
+let emulator = new CHIP8_Emulator();
+let canvas, ctx;
+let stopFlag = false;
 
 window.onload = function() {
-  let fileInput = document.getElementById('fileInput');
-  let canvas = document.getElementById('canvas');
+  fileInput = document.getElementById('fileInput');
+  startButton = document.getElementById('start');
+  stopButton = document.getElementById('stop');
+  nextButton = document.getElementById('next');
+  canvas = document.getElementById('canvas');
   canvas.width = 640;
   canvas.height = 320;
-  let ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
 
   fileInput.addEventListener('change', function(e) {
     let file = fileInput.files[0];
@@ -13,29 +24,97 @@ window.onload = function() {
     let reader = new FileReader();
 
     reader.onload = function(e) {
-      // console.log(reader.result.data);
-      let emulator = new CHIP8_Emulator()
-
+      emulator = new CHIP8_Emulator();
       emulator.loadProgram(reader.result);
-      if (emulator.step() == 1) {
-        drawDisplayToCanvas(ctx, emulator.getDisplay());
-      } else {
-
-      }
-
     }
 
     reader.readAsArrayBuffer(file);
   });
+
+
+  startButton.addEventListener('click', () => { start() })
+  stopButton.addEventListener('click', () => { stop() })
+  nextButton.addEventListener('click', () => {{
+    emulator.step()
+    drawDisplayToCanvas(ctx, emulator.getDisplay());
+
+    console.log(emulator);
+  }})
+
+  document.addEventListener('keydown', (e) => {
+    let keyValue = parseInt(e.key, 16);
+    if (keyValue >= 0 && keyValue <= 15) {
+      emulator.setKey(keyValue, 1)
+    }
+  })
+
+  document.addEventListener('keyup', (e) => {
+    let keyValue = parseInt(e.key, 16);
+    if (keyValue >= 0 && keyValue <= 15) {
+      emulator.setKey(keyValue, 0)
+    }
+  })
+
+
+
+}
+
+function start() {
+  console.log("Starting runtime...")
+  stopFlag = false;
+
+  drawDisplayToCanvas(ctx, emulator.getDisplay());
+
+  let fps = 15;
+  let now;
+  let elapsed;
+
+  let fpsInterval = 1000 / fps;
+  let then = Date.now();
+
+  var animate = () => {
+
+    if (stopFlag) {
+      return;
+    }
+    
+    // request another frame
+    requestAnimationFrame(animate);
+  
+    // calc elapsed time since last loop
+    now = Date.now();
+    elapsed = now - then;
+  
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+        // Get ready for next frame by setting then=now, but...
+        // Also, adjust for fpsInterval not being multiple of 16.67
+        then = now - (elapsed % fpsInterval);
+  
+        // draw stuff here
+        emulator.step()
+        drawDisplayToCanvas(ctx, emulator.getDisplay());
+  
+    }
+  }
+  
+  animate();
+}
+function stop() {
+  stopFlag = true;
+  console.log("Runtime stopped.")
 }
 
 function drawDisplayToCanvas(ctx, display) {
   let cellSize = 10;
 
-  ctx.fillStyle = "black";
   for (let y = 0; y < 32; y++) {
     for (let x = 0; x < 64; x++) {
-      if (display[y][x] == 0) continue;
+      if (display[y][x] == 0) {
+        ctx.fillStyle = "white"
+      } else {
+        ctx.fillStyle = "black"
+      }
       ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize)
     }
   }
